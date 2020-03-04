d18ad33c81190d9ce16525063ae550b747c1101f
Kate Fenchenko <fev3@mail.ru>
2020-03-04 15:44:04 +0300

    home page done

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,25 @@
 import React, {useContext, useState} from 'react'
 import {AlertContext} from './../context/alert/AlertContext'
+import {GithubContext} from './../context/github/githubContext'
 
 export const Search = () => {
     const [value, setValue] = useState('')
-    const {show} = useContext(AlertContext)
+    const alert = useContext(AlertContext)
+    const github = useContext(GithubContext)
     const onSubmit = event => {
         if (event.key !== 'Enter') {
             return
             /* show('Test alert')
             console.log(value) */
         }
+        github.clearUsers()
         if(value.trim()){
-            console.log('Make request with: ', value)
+            alert.hide()
+            github.search(value.trim())
+
+            //console.log('Make request with: ', value)
         } else {
-            show("Введите данные пользователя...")
+            alert.show("Введите данные пользователя...")
         }
          
     }
